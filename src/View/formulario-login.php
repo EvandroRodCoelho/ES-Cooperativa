143d686e6b7e8d8c779423983b91a2c5c6d477ae
Gustavo Rodrigues da Silva <109097884+GustavoRodriguesg@users.noreply.github.com>
2023-11-18 13:31:48 -0300

    atualização no formulario de login

--- src/View/formulario-login.php
+++ src/View/formulario-login.php
@@ -1,16 +1,18 @@
 <?php
 require_once __DIR__ . '/htmlStart.php';
 ?>
-
+<link rel="stylesheet" href="css/formulario-login.css">
 <form method="post">
-    <label for="email">E-mail:
+    <img src="/img/logo.png" alt="" id="logo">
+    <label for="email">
         <input name="email" required id="email" placeholder="Digite seu e-mail"/>
     </label>
-    <label for="senha">Senha:
+    <label for="senha">
         <input name="senha" required id='senha' placeholder="Digite sua senha"/>
     </label>
 
-    <input type="submit" value="Fazer login"/>
+    <input type="submit" value="Faxer login"/>
+    <a href="#">Cadastre-se</a>
 </form>
 
 <?php
